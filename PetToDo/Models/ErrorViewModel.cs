8dbce7ad36e35a0e48d9cba202f4f7e815baaddd
Pavel Ostroukhov <pavel.ostroukhov@gmail.com>
2020-02-18 20:44:39 +0400

    Добавьте файлы проекта.

---
+++ PetToDo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetToDo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
